fbfc321e95bc135698e36b60b7b4f4ea208cb92f
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2023-04-12 19:56:20 +0200

    about-section
    Centered text and added a paragraph.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,6 +10,21 @@ const About = () => {
       <h1>C'est quoi Studios RH ?</h1>
 
       <img src={memoji} alt="memoji" className="memoji" />
+      <p>
+        Lorem ipsum dolor sit amet. Et dolor esse cum quisquam molestias qui
+        sunt inventore qui voluptatem doloribus. Ut quaerat ratione et quos
+        magni et iusto sequi non deleniti consequuntur non dolores atque aut
+        error quaerat est harum accusamus.
+      </p>
+      <p>
+        Id quia eius et labore autem aut perspiciatis nihil est voluptas natus.
+        Hic molestias accusantium est repellendus optio quo beatae quos in quia
+        facere At iusto iusto et omnis delectus. Ea ducimus ratione in velit
+        praesentium eum ipsum corrupti et consequatur omnis sit exercitationem
+        beatae est minima enim et magnam voluptatum. Qui neque sunt est soluta
+        vitae et impedit fugiat aut eaque aperiam non cumque officia sit nihil
+        magnam ut voluptas nihil.
+      </p>
     </section>
   );
 };
